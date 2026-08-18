227940d79fb087057379eac93129738a57596337
bhher30 <bhher30@gmail.com>
2026-08-18 17:45:30 +0900

    a0818

---
+++ java/Hello/src/a0814/streamEx/Trader.java
@@ -0,0 +1,21 @@
+package a0814.streamEx;
+//상인
+public class Trader {
+    private String name;
+    private String city;
+    
+    public Trader(String name, String city) {
+        this.name = name;
+        this.city = city;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getCity() {
+        return city;
+    }
+    @Override
+    public String toString() {
+        return "Trader [name=" + name + ", city=" + city + "]";
+    };
+}
---
+++ java/Hello/src/a0818/movie/Ticket.java
@@ -0,0 +1,16 @@
+package a0818.movie;
+
+/**
+ * Ticket
+ */
+public class Ticket {
+    private ReservationManager reservationManager; //예약관리자
+    
+    //기존 예약 관리자를 받아서 사용하도록 변경
+    public Ticket(ReservationManager reservationManager) {
+        this.reservationManager = reservationManager;
+    }
+
+   
+    
+}
